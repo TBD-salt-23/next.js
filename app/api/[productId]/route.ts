9745066c8fedd770514ed6c2972d837e4f8c9a77
snygghugo <karlhugodahlgren@gmail.com>
2023-07-11 12:11:55 +0200

    Feat: Accessing mongodb through endpoint

---
+++ app/api/[productId]/route.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest } from 'next';
+import { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { Product } from '@/app/db/db';
+type GetParams = {
+  productId: string;
+};
+
+const getProductById = async (productId: string) => {
+  try {
+    console.log('this is the product id we feed to mongoose', productId);
+
+    // const res = await Products_Collection.findById(productId).exec();
+    // const res = await PRODUCT.findById(productId);
+    const res = await Product.findById(productId);
+    console.log('here is our result!', res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const GET = async (
+  req: NextApiRequest,
+  context: { params: GetParams }
+) => {
+  const { productId } = context.params;
+  console.log('Here comes the product id', productId);
+
+  const res = await getProductById(productId);
+  console.log('log in route', res);
+  return NextResponse.json(res);
+};
---
+++ app/db/db.ts
@@ -0,0 +1,27 @@
+import {
+  Schema,
+  model,
+  models,
+  connect,
+  ConnectionStates,
+  ObjectId,
+} from 'mongoose';
+
+const CREDENTIALS = process.env.CREDENTIALS;
+export const CONNECTION = `mongodb+srv://karlhugodahlgren:DtSGySooKatH3RmT@salt-things.9y2hgm9.mongodb.net/products-database?retryWrites=true&w=majority`;
+
+type ProductT = {
+  name: string;
+  quantity: number;
+};
+
+const productSchema = new Schema<ProductT>({
+  name: { type: String, required: true },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const Product =
+  models.Product || model<ProductT>('product', productSchema);
